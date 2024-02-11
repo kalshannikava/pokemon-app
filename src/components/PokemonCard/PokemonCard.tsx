287cb0497b7c9c8fda4a2e1b2b9236f561439096
k.alshannikava <olshannikovastw@gmail.com>
2024-02-11 11:58:22 +0100

    Provide unique keys

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 const PokemonCard = ({ pokemon }: Props) => {
-  const { name, sprites, types, species, height, weight, abilities, stats } = pokemon;
+  const { id, name, sprites, types, species, height, weight, abilities, stats } = pokemon;
   return (
     <div className={styles.PokemonCard}>
       <div className={styles.PokemonInfoContainer}>
@@ -19,11 +19,11 @@ const PokemonCard = ({ pokemon }: Props) => {
           <h3 className={styles.PokemonName}>{`${name} data`}</h3>
           <dl className={classnames(styles.PokemonInfo, styles.Table)}>
             <dt>National №</dt>
-            <dd>{String(pokemon.id).padStart(3, '0')}</dd>
+            <dd>{String(id).padStart(3, '0')}</dd>
             <dt>Type</dt>
             <dd className={styles.PokemonType}>
               {types.map((type) => (
-                <span key={type.type.name} className={styles[type.type.name]}>{type.type.name}</span>
+                <span key={`${type.type.name}_${id}`} className={styles[type.type.name]}>{type.type.name}</span>
               ))}
             </dd>
             <dt>Species</dt>
@@ -34,7 +34,7 @@ const PokemonCard = ({ pokemon }: Props) => {
             <dd>{formatWeight(weight)}</dd>
             <dt>Abilities</dt>
             <dd className={styles.PokemonAbilities}>
-              {abilities.map(ability => <span key={ability.ability.name}>{ability.ability.name}</span>)}
+              {abilities.map(ability => <span key={`${ability.ability.name}_${ability.slot}_${id}`}>{ability.ability.name}</span>)}
             </dd>
         </dl>
         </div>
@@ -43,7 +43,7 @@ const PokemonCard = ({ pokemon }: Props) => {
         <h3 className={styles.StatsHeader}>Base stats</h3>
         <dl className={classnames(styles.PokemonStats, styles.Table)}>
           {stats.map(stat => {
-            return <React.Fragment key={stat.stat.name}>
+            return <React.Fragment key={`${stat.stat.name}_${id}`}>
             <dt>{formatStatName(stat.stat.name)}</dt>
             <dd>
               <span>{stat.base_stat}</span>
