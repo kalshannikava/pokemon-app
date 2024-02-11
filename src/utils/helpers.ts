86e8439a16019b3125b27590180f7748e5d97883
k.alshannikava <olshannikovastw@gmail.com>
2024-02-11 13:59:29 +0100

    Add filtering helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,10 @@
+import { Pokemon } from "../types/pokemon"
+
 /**
  * 
- * @param array Array to split
- * @param itemsPerChunk Number of array items per chunk
- * @returns Chunked array
+ * @param array Array to split.
+ * @param itemsPerChunk Number of array items per chunk.
+ * @returns Chunked array.
  */
 export const chunkArray = (array: Array<any>, itemsPerChunk: number = 20): Array<any>[] => {
   return array.reduce((resultArray, item, index) => { 
@@ -17,3 +19,12 @@ export const chunkArray = (array: Array<any>, itemsPerChunk: number = 20): Array
     return resultArray;
   }, [])
 }
+
+/**
+ * 
+ * @param pokemons Pokemons array to filter.
+ * @param type Type to be filtered by.
+ * @returns Filtered Pokemons.
+ */
+export const filterPokemonsByType = (pokemons: Pokemon[], type: string): Pokemon[] =>
+  pokemons.filter(pokemon => pokemon.types.find(t => t.type.name === type));
