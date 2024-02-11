fb363ac5d6c2fb68bf2d5d855e34eaac814793be
k.alshannikava <olshannikovastw@gmail.com>
2024-02-11 12:23:41 +0100

    Batch requests

--- src/hooks/useFetchPokemons.ts
+++ src/hooks/useFetchPokemons.ts
@@ -1,42 +1,57 @@
 import { useState, useEffect } from 'react';
 
-import { GET_POKEMON } from '../constants';
+import { FETCH_POKEMONS_DETAILS_LIMIT, GET_POKEMON } from '../constants';
 import { FETCH_POKEMONS_OFFSET_INIT } from '../constants';
 import { FETCH_POKEMONS_LIMIT_INIT } from '../constants';
 import type { Pokemon, PokemonDetails, PokemonPartial } from '../types/pokemon';
-import type { GetPoklemonsAdditionalData } from '../types/shared';
+import { chunkArray } from '../utils/helpers';
 
 const useFetchPokemons = (): 
-{ pokemons: Pokemon[], additionalData: GetPoklemonsAdditionalData, fetchPokemons: Function } => {
+{ pokemons: Pokemon[], count: number, isLoading: boolean, fetchPokemons: Function } => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-  const [additionalData, setAdditionalData] = useState<GetPoklemonsAdditionalData>({ count: 0, next: '' });
+  const [count, setCount] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchPokemonsNames = async (url?: string): Promise<PokemonPartial[]> => {
     const queryParams = new URLSearchParams({
       limit: String(FETCH_POKEMONS_LIMIT_INIT),
       offset: String(FETCH_POKEMONS_OFFSET_INIT),
     });
-
     try {
+      setIsLoading(true);
       const response = await fetch(url || `${GET_POKEMON}?${queryParams}`);
       const pokemonsList = await response.json();
-      setAdditionalData({ next: pokemonsList.next, count: pokemonsList.count });
+      setCount(pokemonsList.count);
       return pokemonsList.results;
     } catch (error) {
+      setIsLoading(false);
       console.error('Error fetching pokemons: ', error);
       return [];
     }
   };
 
+  const fetchBatchPokemonsDetails = async (batches: string[]) => {
+    const batchRes = await Promise.all(batches.map(url => fetch(url)));
+    return batchRes;
+  }
+
   const fetchPokemonsNamesDetails = async (pokemonsList: PokemonPartial[]): Promise<PokemonDetails[]> => {
     try {
-      const requests = pokemonsList.map((pokemon) => fetch(pokemon.url));
-      const responses = await Promise.all(requests);
+      const requestsUrls: string[] = pokemonsList.map((pokemon) => pokemon.url);
+      const batches: string[][] = chunkArray(requestsUrls, FETCH_POKEMONS_DETAILS_LIMIT);
+  
+      let responses: Response[] = [];
+      for (const batch of batches) {
+        const response = await fetchBatchPokemonsDetails(batch);
+        responses.push(...response);
+      }
+
       const responsesJson = await responses.map((res) => res.json());
       const pokemonsDetails = await Promise.all(responsesJson);
 
       return pokemonsDetails;
     } catch (error) {
+      setIsLoading(false);
       console.log('Error fetching pokemon details: ', error);
       return [];
     }
@@ -59,13 +74,14 @@ const useFetchPokemons = ():
     const pokemonsList: PokemonPartial[] = await fetchPokemonsNames(url);
     const pokemonsDetails: PokemonDetails[] = await fetchPokemonsNamesDetails(pokemonsList);
 
+    setIsLoading(false);
     setPokemons([...pokemons, ...normalizeData(pokemonsList, pokemonsDetails)]);
   };
   useEffect(() => {
     fetchPokemons();
   }, []);
 
-  return { pokemons, additionalData, fetchPokemons };
+  return { pokemons, count, isLoading, fetchPokemons };
 };
 
 export default useFetchPokemons;
